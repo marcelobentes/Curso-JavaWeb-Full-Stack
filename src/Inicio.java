ac00d7fbf1005bd10b84b26b2e1ef566c91a75c0
marcelobentes <marcelo.maia.m@gmail.com>
2021-09-04 18:49:36 -0300

    Criando Dialogo de entrada

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,9 +1,23 @@
+import javax.swing.JOptionPane;
 
 public class Inicio {
 
 	public static void main(String[] args) {
 		System.out.println("Testando o GitHubDesktop com o Eclipse.");
 		System.out.println("Editando remoto no GitHub");
+		
+		String nome = JOptionPane.showInputDialog("Informe o nome do usuario.");
+		int resposta = JOptionPane.showConfirmDialog(null,"Usuario "+nome+" esta correto ?");
+		
+		if(resposta == 0) {
+			
+			JOptionPane.showMessageDialog(null, "Usuario confirmado.");
+			
+		}else if (resposta == 1) {
+			JOptionPane.showInputDialog("Informe novamente nome do usuario.");
+		}
+			
+		
 
 	}
 
