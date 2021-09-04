46308f4d9831c1efb471374de74687e8aff4c129
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2021-09-04 15:48:15 -0300

    editando remoto no github

--- src/Inicio.java
+++ src/Inicio.java
@@ -3,6 +3,7 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		System.out.println("Testando o GitHubDesktop com o Eclipse.");
+		System.out.println("Editando remoto no GitHub");
 
 	}
 
