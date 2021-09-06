c98e96675cec36f46fb422edd6028affa5271992
marcelobentes <marcelo.maia.m@gmail.com>
2021-09-06 20:32:30 -0300

    criado pasta cursojava

---
+++ src/cursojava/classes/AlunoMain.java
@@ -0,0 +1,15 @@
+package cursojava.classes;
+
+public class AlunoMain {
+
+	public static void main(String[] args) {
+		/*Objero ainda não existe na memoria*/
+		Aluno aluno1;
+		
+		/*Agora temos um objeto real na memoria*/
+		Aluno aluno2 = new Aluno();
+		
+
+	}
+
+}
