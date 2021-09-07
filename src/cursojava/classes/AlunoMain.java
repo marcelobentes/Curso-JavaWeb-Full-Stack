b4c3b5b251993fd1a32bd5e4b54348f9f0650b42
marcelobentes <marcelo.maia.m@gmail.com>
2021-09-06 21:48:41 -0300

    Set e Get e Metodos

--- src/cursojava/classes/AlunoMain.java
+++ src/cursojava/classes/AlunoMain.java
@@ -4,10 +4,32 @@ public class AlunoMain {
 
 	public static void main(String[] args) {
 		/*Objero ainda não existe na memoria*/
+		@SuppressWarnings("unused")
 		Aluno aluno1;
 		
 		/*Agora temos um objeto real na memoria*/
-		Aluno aluno2 = new Aluno();
+		Aluno aluno2 = new Aluno(null, 0, null, null, null, null, null, null, null, null);
+		
+		aluno2.setNome("Rodrigo");
+		aluno2.setNomeEscola("Escola DAVAR");
+		aluno2.setNomeMae("Eudera");
+		aluno2.setNomePai("Jão");
+		aluno2.setIdade(23);
+		aluno2.setDataNascimento("10/08/2003");
+		aluno2.setDataMatricula("01/01/2022");
+		aluno2.setNumeroCpf("123.456.789.10");
+		aluno2.setRg("2020191");
+		aluno2.setSerieMatriculado("5º");
+		aluno2.setNota1(92.4);
+		aluno2.setNota2(72.4);
+		aluno2.setNota3(76.4);
+		aluno2.setNota4(82.4);
+		System.out.println("O nome do Aluno é "+aluno2.getNome());
+		System.out.println("Nome da escola: "+aluno2.getNomeEscola());
+		System.out.println("Idade: "+aluno2.getIdade());
+		System.out.println("Série: "+aluno2.getSerieMatriculado());
+		
+		System.out.println("A Media da nota: "+aluno2.getMedianota());
 		
 
 	}
